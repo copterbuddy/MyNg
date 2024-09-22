cc5d5eaf9fb1d71c12087df753682355de3eb1ae
copterbuddy <copterbuddy@gmail.com>
2024-09-22 19:07:51 +0700

    set all devs page color bisque

--- src/app/components/devs/devs.component.ts
+++ src/app/components/devs/devs.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+interface subMenu {
+  link: string,
+  text: string
+}
 
 @Component({
   selector: 'app-devs',
   templateUrl: './devs.component.html',
   styleUrls: ['./devs.component.css']
 })
-export class DevsComponent{}
+export class DevsComponent{
+  subMenus: subMenu[] = [
+    {
+      link: '/devs/set-title',
+      text: 'Set Title'
+    },
+    {
+      link: '/devs/output-dynamic-content-with-string',
+      text: 'display dynamic content with string'
+    }
+  ]
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+        .body.style.backgroundColor = 'bisque';
+  }
+}
