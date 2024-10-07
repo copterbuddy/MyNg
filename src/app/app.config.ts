c93b0dd39a3712912002e59fd349526621e9214f
copterbuddy <copterbuddy@gmail.com>
2024-10-07 19:19:09 +0700

    ng add @ngrx/store@17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
-
-  ]
+    provideStore()
+]
 };
