21acb4c8c1356147d72adef3321f6c560422e5e3
copterbuddy <copterbuddy@gmail.com>
2024-10-07 15:44:05 +0700

    add RouterLinkActive

--- src/app/components/shared/pokemon-menu/pokemon-menu.component.ts
+++ src/app/components/shared/pokemon-menu/pokemon-menu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './pokemon-menu.component.html',
   styleUrl: './pokemon-menu.component.css'
 })
